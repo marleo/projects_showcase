a92e6319fbdb080bf54ba916bfba4c01cb6fe460
Mario Leopold <mariole@edu.aau.at>
2020-04-28 21:30:03 +0200

    Added Snake game

---
+++ js/Snake/sketch.js
@@ -0,0 +1,59 @@
+let snake;
+let res = 20;
+let food;
+let w;
+let h;
+
+function setup() {
+  let myCanvas = createCanvas(400, 400);
+  myCanvas.parent("snake");
+  frameRate(10);
+  background(0);
+
+  w = floor(width / res);
+  h = floor(height / res);
+
+  snake = new Snake();
+  foodLocation();
+}
+
+function draw() {
+  scale(res)
+  background(0);
+
+  if(snake.eat(food)){
+    foodLocation();
+  }
+
+  snake.update();
+  snake.show();
+
+  if(snake.endGame()){
+    background(255, 0, 0)
+    print("END GAME");
+    noLoop();
+  }
+
+  fill(255,0,0);
+  rect(food.x, food.y, 1, 1)
+}
+
+
+function foodLocation(){
+  let x = floor(random(w));
+  let y = floor(random(h));
+
+  food = createVector(x, y);
+}
+
+function keyPressed(){
+  if(keyCode === LEFT_ARROW){
+    snake.setDir(-1, 0);
+  } else if(keyCode === RIGHT_ARROW){
+    snake.setDir(1, 0);
+  } else if(keyCode === DOWN_ARROW){
+    snake.setDir(0, 1);
+  } else if(keyCode === UP_ARROW){
+    snake.setDir(0, -1);
+  } 
+}
